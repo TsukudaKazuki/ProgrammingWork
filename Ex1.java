fb375a576d7d42addc6869890a8b0c493451d35a
sakecheeeeee <sakecheeeeee@gmail.com>
2013-06-11 23:49:27 +0900

    Revert "testes"
    This reverts commit 01c8945713b8b1e8ef9e010bca87f57e016752aa.

--- Ex1.java
+++ Ex1.java
@@ -7,7 +7,7 @@ public class Ex1{
         int i=0,sum=0;
         try{
             FileReader filein = new FileReader(args[0]);
-            //うんこちんちんアヘ顔ダブルピース
+            //うんこちんちん
             BufferedReader buffin = new BufferedReader(filein);
             Scanner sc = new Scanner(buffin);
             while(sc.hasNext()){
