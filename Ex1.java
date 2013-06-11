e2c8e6f706b91710c82f999fd855339e69927062
sakecheeeeee <sakecheeeeee@gmail.com>
2013-06-11 22:45:57 +0900

    add EX1,4

---
+++ Ex1.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Ex1{
+	public static void main(String[] args){
+        String[] are = new String[100];
+        int i=0,sum=0;
+        try{
+            FileReader filein = new FileReader(args[0]);
+            BufferedReader buffin = new BufferedReader(filein);
+            Scanner sc = new Scanner(buffin);
+            while(sc.hasNext()){
+                i++;
+                are[i-1] = sc.next();
+            }
+            
+            for(i=0;i<10;i += 2){
+                System.out.println(are[i]+" "+are[i+1]);
+            }
+            
+            for(i=1;i<10;i += 2){
+                
+                sum += Integer.parseInt(are[i]);
+            }
+            
+            System.out.println("合計 "+ sum);
+            
+            for(i=1;i<10;i += 2){
+                System.out.print(are[i]+" ");
+            }
+            
+            System.out.println();
+            
+            buffin.close();
+            
+            
+            
+        }catch(IOException e){
+            System.out.println(e);
+        }
+    }
+}
---
+++ Ex4.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.net.*;
+import java.text.*;
+
+public class Ex4 {
+	public static void main (String[] args)throws IOException{
+		ServerSocket serverS = null;
+		Socket clientS = null;
+		try{serverS = new ServerSocket(50000);//接続要求を持つソケット生成
+		}catch(IOException e){
+			System.out.println("ポート番号にアクセス出来ません。");
+			System.exit(1);
+		}
+		try{clientS = serverS.accept();
+		}catch(IOException e){
+			System.out.println("acceptに失敗しました。");
+			System.exit(1);
+		}
+        
+        PrintWriter out = new PrintWriter(clientS.getOutputStream(),true);
+        Date now = new Date();
+        Format fmt= DateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.LONG);
+        
+
+        out.println(fmt.format(now));
+        out.close();clientS.close();serverS.close();
+		
+	}
+    
+}
