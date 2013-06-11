f5f5d4dab85de003a291d3b86f55d263f1cd669f
sakecheeeeee <sakecheeeeee@gmail.com>
2013-06-12 00:04:27 +0900

    add Black_Jack
    ブラックジャックのアレ
    だめだめなソース(･ัω･ั）

---
+++ BLACK_JACK/dataIO.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+
+public class dataIO {
+	public static void main(String[] args)throws IOException{
+		Socket gameS = null; //ソケット
+		BufferedReader in = null;//ソケットからの入力
+		PrintWriter out = null;//ソケットへの出力
+		try{
+			gameS = new Socket(InetAddress.getLocalHost(),50000);
+			in = new BufferedReader(new InputStreamReader(gameS.getInputStream()));
+			out = new PrintWriter(gameS.getOutputStream(),true);
+		}catch(UnknownHostException e){
+			System.out.println("ホストに接続出来ません。");
+			System.exit(1);
+		}catch(IOException e){
+			System.out.println("IOコネクションを得られません。");
+			System.exit(1);
+		}
+		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+		String fromServer;
+		String fromUser;
+		while((fromServer = in.readLine()) != null){
+			System.out.println(">" + fromServer);//応答を表示
+			if(fromServer.equals("また遊んで下さいね"))break;//サーバが会話を打ち切ったので終了
+			fromUser = stdIn.readLine();
+			
+				if(fromUser == "y" || fromUser == "n"){
+				}else{
+					System.out.println("指定外の文字列です。");
+					fromUser = stdIn.readLine();
+					continue;
+				};
+			out.println(fromUser);
+		}
+		out.close();in.close();stdIn.close();gameS.close();
+	}
+}
---
+++ BLACK_JACK/server.java
@@ -0,0 +1,124 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Random;
+
+public class server {
+	public static void main (String[] args)throws IOException{
+		ServerSocket serverS = null;
+		Socket clientS = null;
+		try{serverS = new ServerSocket(50000);//接続要求を持つソケット生成
+		}catch(IOException e){
+			System.out.println("ポート番号にアクセス出来ません。");
+			System.exit(1);
+		}
+		try{clientS = serverS.accept();
+		}catch(IOException e){
+			System.out.println("acceptに失敗しました。");
+			System.exit(1);
+		}
+
+		//出力ストリーム
+		PrintWriter out = new PrintWriter(clientS.getOutputStream(),true);
+		//入力ストリーム
+		BufferedReader in = new BufferedReader(new InputStreamReader(clientS.getInputStream()));
+		String fromC,fromUser;
+		blackJack b = new blackJack();
+		while((fromUser = in.readLine()) != null){
+			out.println("ゲームを始めます");
+			b.set();
+			b.output();
+			out.println("カードをドローしますか？");
+
+			out.println(fromC);
+		}
+	}
+}
+
+class blackJack {
+	int playerCounter,dealerCounter;
+	int trump[] = new int[52];
+	int dealerCards[] = new int[52];
+	int playerCards[] = new int[52];
+	Random rnd = new Random();
+
+	//////////////////////////////////////////////////////////
+	//すべての変数の初期化.プレイヤーとディーラーにカードを2枚配布する。//
+	//////////////////////////////////////////////////////////
+
+	void set(){
+
+		playerCounter = 0;
+		dealerCounter = 0;
+
+		for(int i=0; i<52; i++){
+			trump[i] = 0;
+			dealerCards[i] = 0;
+			playerCards[i] = 0;
+		}
+
+		for(int i=0; i<52; i++){
+			trump[i] = i % 13 + 1;
+		}
+
+		for(int i=0; i<2; i++){
+			playerCards[i] = this.draw();
+			dealerCards[i] = this.draw();
+		}
+	}
+
+	int draw(){
+		int ran = 0;
+		do{
+			ran = rnd.nextInt(52);
+		}while(trump[ran] == 0);//引いたカードの配列の中身が0なら引き直し
+		int drawCard = trump[ran];
+		trump[ran] = 0;
+
+		return drawCard;
+	}
+
+	//文字出力担当
+
+	void output(){
+		System.out.println("-----------ステータス-----------");
+		out.print("CPU:");
+		for(int i=0; dealerCards[i] != 0 ; i++){
+			if(i==0){
+				if(dealerCards[i] == 1){
+					out.print(" A");
+				}else if(dealerCards[i] == 11){
+					out.print(" J");
+				}else if(dealerCards[i] == 12){
+					out.print(" Q");
+				}else if(dealerCards[i] == 13){
+					out.print(" K");
+				}else{
+					out.print(" " + dealerCards[i]);
+				}
+				out.print(dealerCards[i]);
+			}
+			out.print(" ＊");
+		}
+		out.println();
+		out.print("あなた:");
+		for(int i=0; playerCards[i] != 0; i++){
+			if(playerCards[i] == 1){
+				out.print(" A");
+			}else if(playerCards[i] == 11){
+				out.print(" J");
+			}else if(playerCards[i] == 12){
+				out.print(" Q");
+			}else if(playerCards[i] == 13){
+				out.print(" K");
+			}else{
+				out.print(" " + playerCards[i]);
+			}
+		}
+		out.println();
+		out.println("------------------------------");
+	}
+}
