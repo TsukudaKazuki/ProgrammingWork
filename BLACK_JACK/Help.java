3752c625cb31c6fb875e23debb3e899fdfe1f377
sakecheeeeee <sakecheeeeee@gmail.com>
2013-06-15 19:45:53 +0900

    help実装
    しました！

---
+++ BLACK_JACK/Help.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Help {
+	public static final Boolean TRUE = new Boolean(true);
+	BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+	String in;
+	void help() throws IOException{
+		do{
+			System.out.println("ヘルプです。");
+			System.out.println("見たい項目を選んでください。\n");
+			System.out.println("(1)ルール\n(2)製作者\n(3)見たくない\n");
+
+			do{
+			in = stdIn.readLine();
+
+			if(!(in.equals("1") || in.equals("2") || in.equals("3"))){
+				System.out.println("指定外の入力です。\n");
+			}else{
+				break;
+			}
+
+			}while(TRUE);
+
+			if(in.equals("1")){
+				System.out.println("\n\n\n（１）ルール");
+				System.out.println("　・まず両者にカードを2枚ずつ配布する。 ");
+				System.out.println("　・その後、カードを引くかあがるかを決める。");
+				System.out.println("　・A, J, Q, K以外のカードを引いた時は表記されて いる数字をそのまま持ち点に加算する。");
+				System.out.println("　・Aを引いた場合、持ち点が10以下のときは11 を、11以上のときは1を加算する。");
+				System.out.println("　・J, Q, Kを引いた場合は持ち点を10加算する。");
+				System.out.println("　・交互に繰り返し、どちらかがあがりを宣言した後、両者の点数を比べ、より21に近い方が勝者となる。");
+				System.out.println("　・持ち点が22以上になるとその時点で敗者となる。");
+				System.out.println("\n\n\n以上です。\n\n\n");
+			}else if(in.equals("2")){
+				System.out.println("\nこのゲームは\n\n入出力処理:さけちー\n内部処理:つくっち\n\nの提供でお送りします。");
+			}else if(in.equals("3")){
+				System.out.println("ではゲームを楽しんでください！\n");
+				break;
+			}
+
+
+
+		}while(TRUE);
+
+	}
+}
--- BLACK_JACK/dataIO.java
+++ BLACK_JACK/dataIO.java
@@ -13,53 +13,62 @@ public class dataIO {
 		Socket gameS = null; //ソケット
 		BufferedReader in = null;//ソケットからの入力
 		PrintWriter out = null;//ソケットへの出力
-		try{
-			gameS = new Socket(InetAddress.getLocalHost(),50000);
-			in = new BufferedReader(new InputStreamReader(gameS.getInputStream()));
-			out = new PrintWriter(gameS.getOutputStream(),true);
-		}catch(UnknownHostException e){
-			System.out.println("ホストに接続出来ません。");
-			System.exit(1);
-		}catch(IOException e){
-			System.out.println("IOコネクションを得られません。");
-			System.exit(1);
-		}
-		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-		String fromServer;
-		String fromUser = null;
-		while((fromServer = in.readLine()) != null){
-			System.out.println(">" + fromServer);//応答を表示
-			if(fromServer.equals("また遊んで下さいね!")){
-				System.out.println();
-				break;//サーバが会話を打ち切ったので終了
-			}
+		Help h = new Help();
 
-			if(fromServer.equals("カードをドローしますか？ y...はい | n...いいえ")){
-				do{
-					fromUser = stdIn.readLine();
-					if(fromUser.equals("y") || fromUser.equals("n")){
-						break;
-					}else{
-						System.out.println("指定外の文字列です。");
-						continue;
-					}
-				}while(TRUE);
-			out.println(fromUser);
+		if(args.length > 0)
+		{
+			if(args[0].equals("--help")){
+				h.help();
 			}
+		}else{
+			try{
+				gameS = new Socket(InetAddress.getLocalHost(),50000);
+				in = new BufferedReader(new InputStreamReader(gameS.getInputStream()));
+				out = new PrintWriter(gameS.getOutputStream(),true);
+			}catch(UnknownHostException e){
+				System.out.println("ホストに接続出来ません。");
+				System.exit(1);
+			}catch(IOException e){
+				System.out.println("IOコネクションを得られません。");
+				System.exit(1);
+			}
+			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+			String fromServer;
+			String fromUser = null;
+			while((fromServer = in.readLine()) != null){
+				System.out.println(">" + fromServer);//応答を表示
+				if(fromServer.equals("また遊んで下さいね!")){
+					System.out.println();
+					break;//サーバが会話を打ち切ったので終了
+				}
 
-			if(fromServer.equals("ゲームを続けますか？ y...はい | n...いいえ")){
-				do{
-					fromUser = stdIn.readLine();
-					if(fromUser.equals("y") || fromUser.equals("n") ){
-						break;
-					}else{
-						System.out.println("指定外の文字列です。");
-						continue;
-					}
-				}while(TRUE);
-			out.println(fromUser);
+				if(fromServer.equals("カードをドローしますか？ y...はい | n...いいえ")){
+					do{
+						fromUser = stdIn.readLine();
+						if(fromUser.equals("y") || fromUser.equals("n")){
+							break;
+						}else{
+							System.out.println("指定外の文字列です。");
+							continue;
+						}
+					}while(TRUE);
+					out.println(fromUser);
+				}
+
+				if(fromServer.equals("ゲームを続けますか？ y...はい | n...いいえ")){
+					do{
+						fromUser = stdIn.readLine();
+						if(fromUser.equals("y") || fromUser.equals("n") ){
+							break;
+						}else{
+							System.out.println("指定外の文字列です。");
+							continue;
+						}
+					}while(TRUE);
+					out.println(fromUser);
+				}
 			}
+			out.close();in.close();stdIn.close();gameS.close();
 		}
-		out.close();in.close();stdIn.close();gameS.close();
 	}
 }
