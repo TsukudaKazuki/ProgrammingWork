1f261ca05eb904ee8493c99d9351ffcca121b040
sakecheeeeee <hatakenakajun@jun-mba.local>
2013-06-17 23:15:57 +0900

    add コメント
    これでいけたんちゃう！？

--- BLACK_JACK/dataIO.java
+++ BLACK_JACK/dataIO.java
@@ -13,14 +13,15 @@ import java.net.UnknownHostException;
 public class dataIO {
 	private static final Boolean TRUE = new Boolean(true);
 	public static void main(String[] args)throws IOException{
-		int playMode = 0;//0で通常起動,1で接待プレイ,2でヘルプ,3は不正オプションのため終了
-		Socket gameS = null; //ソケット
-		BufferedReader in = null;//ソケットからの入力
-		PrintWriter out = null;//ソケットへの出力
+		int playMode = 0;			//0で通常起動,1で接待プレイ,2でヘルプ,3は不正オプションのため終了
+		Socket gameS = null;		//ソケット
+		BufferedReader in = null;	//ソケットからの入力
+		PrintWriter out = null;		//ソケットへの出力
 		File file = null;
-		BufferedWriter bw=null;
-		Help h = new Help();
+		BufferedWriter bw=null;		//ファイル出力用
+		Help h = new Help();		//Helpクラス
 
+		//オプション入力判定。--helpでヘルプ。-settaiPlayで接待モード起動。
 		if(args.length > 0)
 		{
 			if(args[0].equals("--help")){
@@ -75,11 +76,12 @@ public class dataIO {
 					break;//サーバが会話を打ち切ったので終了
 				}
 
+				//入力用。文字の不正入力も検知する。
 				if(fromServer.equals("カードをドローしますか？ y...はい | n...いいえ") || fromServer.equals("カードをお渡しいたしましょうか？ y...はい | n...いいえ")){
 					do{
 						fromUser = stdIn.readLine();
 						if(fromUser.equals("y") || fromUser.equals("n")){
-							bw.write(fromUser);
+							bw.write(fromUser);		//ファイルへの書き込み
 							bw.newLine();
 							break;
 						}else{
@@ -94,6 +96,7 @@ public class dataIO {
 					out.println(fromUser);
 				}
 
+				//入力用。文字の不正入力も検知する。
 				if(fromServer.equals("ゲームを続けますか？ y...はい | n...いいえ") || fromServer.equals("引き続きゲームで遊んで頂けますか？ y...はい | n...いいえ")){
 					do{
 						fromUser = stdIn.readLine();
--- BLACK_JACK/server.java
+++ BLACK_JACK/server.java
@@ -6,9 +6,9 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class server {
-	private static final Boolean TRUE = new Boolean(true);
-	public  static PrintWriter out;
-	public static blackJackRoutin b = new blackJackRoutin();
+	private static final Boolean TRUE = new Boolean(true);			//無限ループ用Boolean
+	public  static PrintWriter out;									//クライアントソフトへの出力用
+	public static blackJackRoutin b = new blackJackRoutin();		//ゲームルーチンクラス
 	public static void main (String[] args)throws IOException{
 		ServerSocket serverS = null;
 		Socket clientS = null;
@@ -30,43 +30,52 @@ public class server {
 		String fromUser = null;
 		int playMode=0;
 
-		playMode = Integer.parseInt(in.readLine());//プレイモード読み取り用
+		//プレイモード読み取り用
+		playMode = Integer.parseInt(in.readLine());
 
-		//ゲーム内ループ
+		//通常起動起動した際にヘルプモードがあることをアナウンス
 		if(playMode==0)out.println("ルールが分からない場合はオプションに--helpを付けて起動してヘルプを見て下さいね！");
+		
+		//ゲーム内ループ
 		do{
-			int settaiReStart=0;
-			b.gameStartMassage(playMode);
-			b.set();
-			b.output(playMode);
+			int settaiReStart=0;			//接待プレイ時用フラグ
+			b.gameStartMassage(playMode);	//ゲームスタートのメッセージを出力
+			b.set();						//トランプ、プレイヤーの手札、ディーラーの手札の初期化
+			b.output(playMode);				//ステータスを出力
+			
+			//ドローを繰り返すための無限ループ。バースト(点数が21を超える）するか非ドローを選択するとループから抜ける。
 			while(TRUE){
-				b.drawMessage(playMode);
+				b.drawMessage(playMode);	//ドローするか否かのメッセージを表示
 
-				fromUser = in.readLine();
+				fromUser = in.readLine();	//入力待ち
 
-				if(fromUser.equals("y")){
+				if(fromUser.equals("y")){	//入力が"y"ならドローを続ける
 					b.playerDraw();
 					b.output(playMode);
 				}else{
 					break;
 				}
 
+				/*バーストしていない場合はドローの続き、
+				バーストしていればループから抜ける。
+				playModeが1（接待プレイ）のときバーストしていれは、
+				settaiRestartに2が入りゲームスタートからやり直し*/
 				settaiReStart = b.burstJudge(playMode);
 				if(settaiReStart != 0)break;
 
 			}
 
-			if(settaiReStart == 2)continue;
+			if(settaiReStart == 2)continue;	//ここでループやり直し
 
-			b.turnOfDealer(playMode);
-			b.dealerAction(playMode);
-			b.outputResult(playMode);
-			b.judge(playMode);
-			b.gameContinue(playMode);
+			b.turnOfDealer(playMode);		//ディーラーのターンであることを表示する
+			b.dealerAction(playMode);		//ディーラーのドロー等のアクションを行う
+			b.outputResult(playMode);		//CPUの手札、点数を含めたステータスの表示を行う
+			b.judge(playMode);				//勝敗判定を行い、どちらの勝利かを表示する
+			b.gameContinue(playMode);		//ゲームの続行意思の確認。y入力でもう一度ループの先頭へ、nでループから抜ける。
 			fromUser = in.readLine();
 		}while(fromUser.equals("y"));
 
-		b.endMassage(playMode);
+		b.endMassage(playMode);				//ゲーム終了メッセージの表示
 		out.close();clientS.close();serverS.close();
 	}
 }
